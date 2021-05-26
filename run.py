747732d4f96684f4bddbea5001293662bbe85eae
Sasha <successamaz@gmail.com>
2021-05-26 16:55:04 +0300

    Flask service

--- run.py
+++ run.py
@@ -12,6 +12,7 @@ from utils import (
 	get_bill_sign, 
 	get_invoice_sign
 )
+from datetime import datetime
 import logging
 import os
 import requests
@@ -27,8 +28,21 @@ logging.basicConfig(
 	level=logging.INFO,
 	format='%(asctime)s | %(levelname)s | %(name)s | %(message)s'
 )
-from models import Payment
 
+
+# Payment model
+class Payment(db.Model):
+		id = db.Column(db.Integer, primary_key=True)
+		amount = db.Column(db.String(255), nullable=False)
+		currency = db.Column(db.String(3), nullable=False)
+		description = db.Column(db.Text, nullable=False)
+		timestamp = db.Column(db.DateTime, default=datetime.now)
+
+		def __repr__(self):
+				return f'Payment({self.amount}, {self.currency}, {self.timestamp})'
+
+
+# Routes
 @app.route('/', methods=['GET', 'POST'])
 def payment():
 		form = PaymentForm()
