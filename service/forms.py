072176db01eda58c66715e288fe750795eb4f9c8
Sasha <successamaz@gmail.com>
2021-08-08 14:44:34 +0300

    Flask service

--- service/forms.py
+++ service/forms.py
@@ -13,32 +13,32 @@ from wtforms.validators import (
 
 
 class PaymentForm(FlaskForm):
-		amount = DecimalField(
-			'Amount:',
-			validators=[DataRequired()], 
-			default=10.00
-		)
-		currency = SelectField(
-			'Currency:', 
-			choices=[
-				('978', 'EUR'), 
-				('840', 'USD'), 
-				('643', 'RUB')
-			]
-		)
-		description = TextAreaField(
-			'Description:', 
-			validators=[DataRequired()], 
-			default='Test invoice'
-		)
-		shop_id = HiddenField(default='5')
-		shop_order_id = HiddenField(default='101')
-		submit = SubmitField('Pay')
+	amount = DecimalField(
+		'Amount:',
+		validators=[DataRequired()], 
+		default=10.00
+	)
+	currency = SelectField(
+		'Currency:', 
+		choices=[
+			('978', 'EUR'), 
+			('840', 'USD'), 
+			('643', 'RUB')
+		]
+	)
+	description = TextAreaField(
+		'Description:', 
+		validators=[DataRequired()], 
+		default='Test invoice'
+	)
+	shop_id = HiddenField(default='5')
+	shop_order_id = HiddenField(default='101')
+	submit = SubmitField('Pay')
 
-		def validate_amount(self, amount):
-				value = str(amount.data)
-				if '.' not in value:
-						raise ValidationError('Error! Correct format: 100.00')
-				items = value.rsplit('.')
-				if len(items[1]) > 2 or len(items[1]) < 2:
-						raise ValidationError('Error! Correct format: 100.00')
+	def validate_amount(self, amount):
+		value = str(amount.data)
+		if '.' not in value:
+			raise ValidationError('Error! Correct format: 100.00')
+		items = value.rsplit('.')
+		if len(items[1]) > 2 or len(items[1]) < 2:
+			raise ValidationError('Error! Correct format: 100.00')
--- service/models.py
+++ service/models.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 
 class Payment(db.Model):
-		id = db.Column(db.Integer, primary_key=True)
-		amount = db.Column(db.String(255), nullable=False)
-		currency = db.Column(db.String(3), nullable=False)
-		description = db.Column(db.Text, nullable=False)
-		timestamp = db.Column(db.DateTime, default=datetime.now)
+	id = db.Column(db.Integer, primary_key=True)
+	amount = db.Column(db.String(255), nullable=False)
+	currency = db.Column(db.String(3), nullable=False)
+	description = db.Column(db.Text, nullable=False)
+	timestamp = db.Column(db.DateTime, default=datetime.now)
 
-		def __repr__(self):
-				return f'Payment({self.amount}, {self.currency}, {self.timestamp})'
+	def __repr__(self):
+		return f'Payment({self.amount}, {self.currency}, {self.timestamp})'
